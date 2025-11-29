9834a81b864dfa78b5cf97cc665af08f94ea214d
Evgeny2723 <96776434+Evgeny2723@users.noreply.github.com>
2025-11-29 15:50:49 +0500

    Update universal.js

--- universal.js
+++ universal.js
@@ -60,6 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let iti = null;
     let isSubmitting = false;
     let isFormInitialized = false;
+    codeFormContainer.style.display = 'none';
 
     // Honeypot переменные
     let formInteractionStartTime = 0;
