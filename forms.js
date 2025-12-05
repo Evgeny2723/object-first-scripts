12d0567062f7d861e6459640f275742285edf607
Evgeny2723 <96776434+Evgeny2723@users.noreply.github.com>
2025-12-05 13:12:22 +0500

    Update forms.js

--- forms.js
+++ forms.js
@@ -711,6 +711,11 @@ document.addEventListener('DOMContentLoaded', function() {
                                     setTimeout(() => {
                                         const targetId = stateInputIds[data.country];
                                         const stateSelect = document.getElementById(targetId);
+
+                                        console.log('Попытка автозаполнения для:', data.country);
+                                        console.log('Ищем ID:', targetId);
+                                        console.log('Найден элемент:', stateSelect);
+                                        console.log('Данные API:', data.state_name, data.state, data.city);
                                         
                                         if (stateSelect) {
                                             const foundOption = findBestOption(
